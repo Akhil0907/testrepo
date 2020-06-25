b64e121b2f8d59f56c2b9b0d2526f6616b432ed0
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2020-06-25 17:51:54 +0530

    minor

--- cypress/integration/Smoke/Campaigns/createSimpleCampaign.spec.js
+++ cypress/integration/Smoke/Campaigns/createSimpleCampaign.spec.js
@@ -37,39 +37,6 @@ describe('Verify the useriq web dashbord is working fine', () => {
         cy.contains('Campaigns').click({force:true})
 
         simpleCampaigns.getCreateCampaignButton().should('be.visible').click({force:true})
-        
-        //selecting simple text only Announcement campaigns
-        simpleCampaigns.selectCampaign().eq(0).click()
-
-        simpleCampaigns.simpleText().should('be.visible').click()
-        
-        //content tab
-
-
-        simpleCampaigns.selectCampaignItems().eq(0).should('not.be.disabled').click().type('simpleCampaign12')
-
-        simpleCampaigns.selectCampaignItems().eq(1).click()
-
-        cy.contains('UserIQ Default').should('be.visible').click()
-
-        simpleCampaigns.selectCampaignItems().eq(2).should('not.be.disabled').click().clear().type('Smoke testing ')
-
-        //simpleCampaigns.getCampaignDescription().click({force:true}).clear().type('This is description area')
-
-        //segment tab
-        cy.contains('Segment').should('not.be.disabled').click()
-
-        simpleCampaigns.selectSegment().should('not.be.hidden').click({multiple:true})
-        
-        //extra delay for respomse to load
-        Cypress.config('responseTimeout',5000)
-
-        cy.contains('Active Accounts').click()
-        
-        //review tab
-        simpleCampaigns.reviewButton().should('be.visible').click()
-
-        cy.contains('Save Campaign').should('be.visible').click()
        
     })
        
--- cypress/pageObjects/Campaigns/CreateCampaigns/createAnnouncementCampaigns.js
+++ cypress/pageObjects/Campaigns/CreateCampaigns/createAnnouncementCampaigns.js
@@ -12,66 +12,6 @@ class SimpleCampaigns
    
    //to use the cypress xpath synctax just type npm install -D cypress-xpath
    // in cypress home directory of your project
-   simpleText()
-   {
-       return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[1]")
-
-   }
-
-   focussedText()
-   {
-        return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[2]/div/div")
-   }
- 
-   focussedBullets()
-   {
-
-      return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[3]/div/div")
-   }
-
-   detailedBullets()
-   {
-
-      return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[4]/div/div")
-   }
-   
-   detailedTwoBlock()
-
-   {
-      return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[5]/div/div")
-      
-   }
-
-   detailedFourBlock()
-
-   {
-      return cy.xpath("//*[@id='toolbar']/div[2]/div/div/div[5]/div/div")
-      
-   }
-   selectCampaignItems()
-   {
-
-    return cy.get('.MuiInputBase-input')
-   }
-
-   getCampaignDescription()
-   {
-
-    return cy.get('.redactor-in')
-   }
-
-   selectSegment()
-   {
-
-    return cy.xpath("//input[@placeholder='Select a Segment']")
-   }
-
-   reviewButton()
-
-   {
-
-    return cy.xpath("//button[@data-id='uiq-114']")
-   }
-}
+  
 
 export default SimpleCampaigns;
